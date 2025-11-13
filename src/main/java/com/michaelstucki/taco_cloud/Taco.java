559aea10640dd3b58634c256752e04c5c287b114
michaelstucki <mstucki7@icloud.com>
2025-11-12 19:14:28 -0700

    add other domains: Taco, TacoOrder

---
+++ src/main/java/com/michaelstucki/taco_cloud/Taco.java
@@ -0,0 +1,11 @@
+package com.michaelstucki.taco_cloud;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class Taco {
+
+    private String name;
+    private List<Ingredient> ingredients;
+}
---
+++ src/main/java/com/michaelstucki/taco_cloud/TacoOrder.java
@@ -0,0 +1,24 @@
+package com.michaelstucki.taco_cloud;
+
+import lombok.Data;
+import java.util.List;
+import java.util.ArrayList;
+
+@Data
+public class TacoOrder {
+
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addTaco(Taco taco) {
+        tacos.add(taco);
+    }
+}
