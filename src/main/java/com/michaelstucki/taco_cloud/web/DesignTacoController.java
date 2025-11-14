6c150ac6851dc2516937b919bb58b7ca72e3601f
michaelstucki <mstucki7@icloud.com>
2025-11-13 19:52:48 -0700

    add controller for design (a taco) endpoint

---
+++ src/main/java/com/michaelstucki/taco_cloud/web/DesignTacoController.java
@@ -0,0 +1,74 @@
+package com.michaelstucki.taco_cloud.web;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.michaelstucki.taco_cloud.Ingredient;
+import com.michaelstucki.taco_cloud.Ingredient.Type;
+import com.michaelstucki.taco_cloud.Taco;
+import com.michaelstucki.taco_cloud.TacoOrder;
+
+// Generate a Simple Logging Facade for Java Logger (to be used later)
+@Slf4j
+// This class is a Controller, autoconfiguration will scan for it and create its bean
+@Controller
+// This class is mapped to an HTTP request to this endpoint (this path)
+@RequestMapping("/design")
+// Persist the TacoOrder bean for the duration of the client's session (bean scope)
+@SessionAttributes("tacoOrder")
+// When the endpoint is called, this method is run
+public class DesignTacoController {
+
+    // Spring MVC adds the list of ingredients to the Model instance
+    // A Model instance ferries data from the controller to the view
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
+
+        Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+    }
+
+    // Thymeleaf uses Iterable in its th:each HTML tag attribute
+    private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+
+    // Spring MVC adds an empty TacoOrder instance to the Model
+    @ModelAttribute(name = "tacoOrder")
+    public TacoOrder order() { return new TacoOrder(); }
+
+    // Spring MVC adds an empty Taco instance to the Model
+    @ModelAttribute(name = "taco")
+    public Taco taco() { return new Taco(); }
+
+    // A GET HTTP request to /path results in this method being called
+    // This method returns the logical name of the view (i.e., the design.html file)
+    @GetMapping
+    public String showDesignForm() { return "design"; }
+}
